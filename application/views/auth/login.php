8733f75cf26e771b3941c200ac9d395ad4feac19
unknown <fpermana275@gmail.com>
2023-07-16 20:18:46 +0700

    init

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -11,7 +11,7 @@
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center mb-4">
-                                <h1 class="h4 text-gray-900"><span>Sistem Informasi</span> Pengelolaan Barang</h1>
+                                <h1 class="h4 text-gray-900"><span>Sistem Informasi</span> Persediaan Barang</h1>
                                 <span class="text-muted">Login</span>
                             </div>
                             <?= $this->session->flashdata('pesan'); ?>
